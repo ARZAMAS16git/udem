b5e6baee63891e55259a0e9b50d735858ab0b448
Alex <alex666che@mail.ru>
2021-01-06 02:22:35 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+
+"use  strict";
+/*const result = confirm ("Are you here?");
+
+if (result == true){
+    alert('давай работать ')
+}
+else{
+    alert('досвидание')
+};
+*/
+/*const answer = prompt('вам есть 18', '');
+console.log(answer+5);*/
+// const answer = [];
+// answer[1]= prompt(' как ваше фамилия','');
+// answer[2]= prompt(' сколько вам лет','');
+
+// document.write(answer);
+// console.log(answer[1]);
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+const user = 'Ivan';
+alert(`Привет,${user}`)
